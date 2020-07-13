9faca0d13119ade61ee3d871033ad02cd3db57d1
11fsy <55930961+11fsy@users.noreply.github.com>
2020-07-13 16:06:59 +0200

    Update plot 4.R

--- plot 4.R
+++ plot 4.R
@@ -1,7 +1,7 @@
 data11 <- read.table("assignment 1.txt", header = TRUE, sep = ";", na.strings = "?")
 data <- rbind(data11[data11$Date == "1/2/2007", ], data11[data11$Date == "2/2/2007", ])
 data$DateTime <- strptime(paste(data$Date, data$Time, sep = ""), "%d/%m/%Y %H:%M")
-png("plot4.png", width=480, height=480)
+png("4.png", width=480, height=480)
 par(mfrow = c(2, 2))
 plot(data$DateTime, data$Global_active_power, xlab = "", ylab = "Global Active power", type = "l")
 plot(data$DateTime, data$Voltage, xlab = "datatime", ylab = "Voltage", type = "l")   
