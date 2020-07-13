dc8217156bc4aa4286b5c5b1013ebc9e2579cb80
11fsy <55930961+11fsy@users.noreply.github.com>
2020-07-13 16:03:53 +0200

    Add files via upload

---
+++ plot 1.R
@@ -0,0 +1,6 @@
+data11 <- read.table("assignment 1.txt", header = TRUE, sep = ";", na.strings = "?")
+data <- rbind(data11[data11$Date == "1/2/2007", ], data11[data11$Date == "2/2/2007", ])
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+png("plot1.png",width = 480, height = 480)
+hist(data$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+dev.off()
