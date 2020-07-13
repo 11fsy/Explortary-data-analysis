9b4896a2893ca936e9c965b723f569fb699c60c9
11fsy <55930961+11fsy@users.noreply.github.com>
2020-07-13 16:02:38 +0200

    Delete plot 2.R

--- plot 2.R
+++
@@ -1,6 +0,0 @@
-data11 <- read.table("assignment 1.txt", header = TRUE", sep = ";", na.strings = "?")
-data <- rbind(data11[data11$Date == "1/2/2007", ], data11[data11$Date == "2/2/2007", ])
-data$DateTime <- strptime(paste(data$Date, data$Time, sep = ""), "%d/%m/%Y %H:%M")
-png("plot2.png", width=480, height=480)
-plot(data$DateTime, data$Global_active_power, xlab = "", ylab = "Global Active power(kilowatts)", type = "l")
-dev.off()
